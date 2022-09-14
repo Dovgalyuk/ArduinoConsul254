21b719dca8bc15b52c0761c8950e7f52c22d8973
Pavel Dovgalyuk <pavel.dovgaluk@gmail.com>
2022-09-14 09:32:52 +0300

    Implemented Consul-to-LCD data transfer

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -1,14 +1,14 @@
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_PCF8574.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);
+LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27
 
 char table[256];
 
 void fill_table()
-{
+{  
   // некоторые символы не существуют в ASCII, некоторых не разобрать на скане
   table[B11111000] = '-'; //
-  table[B11111011] = 'А'; //
+  table[B11111011] = 'A'; //
   table[B01111001] = '%'; //
   table[B01111010] = 'Б'; //
   table[B00010000] = '/'; // знак деления
@@ -16,7 +16,7 @@ void fill_table()
   table[B11011001] = 'D'; //
   table[B11011010] = 'Д'; //
   table[B01011000] = ':'; //
-  table[B01011011] = 'Е'; //
+  table[B01011011] = 'E'; //
   table[B11010000] = 'F'; //
   table[B11010011] = 'Ф'; //
   table[B00111000] = 'G'; //
@@ -32,11 +32,11 @@ void fill_table()
   table[B00101001] = 'L'; //
   table[B00101010] = 'Л'; //
   table[B11001000] = '#'; // знак тождественности
-  table[B11001011] = 'М'; //
+  table[B11001011] = 'M'; //
   table[B01001001] = 'N'; //
-  table[B01001010] = 'Н'; //
+  table[B01001010] = 'H'; //
   table[B10001001] = '#'; // знак отрицания
-  table[B10001010] = 'О'; //
+  table[B10001010] = 'O'; //
   table[B00001000] = '#'; // знак надмножества
   table[B00001011] = 'П'; //
   table[B10000000] = 'Q'; //
@@ -44,7 +44,7 @@ void fill_table()
   table[B11110001] = 'R'; //
   table[B11110010] = 'Г'; //
   table[B01110000] = 'S'; //
-  table[B01110011] = 'С'; //
+  table[B01110011] = 'C'; //
   table[B10110000] = 'V'; //
   table[B10110011] = 'T'; //
   table[B00110001] = 'U'; //
@@ -109,16 +109,59 @@ void fill_table()
   table[B00110111] = '$'; // черная лента
 }
 
+#define HIGHREG B11010110
+#define LOWREG  B10010111
+
 // Судя по цифрам, разряды будет удобнее развернуть и инвертировать
 
 void setup() {
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("Hello");
-  lcd.setCursor(0,1);
-  lcd.print("ArduinoMaster");
+  // start serial port at 9600 bps and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  fill_table();
+
+  lcd.begin(16, 2);
+  lcd.setBacklight(255);
+  lcd.home();
+  lcd.clear();
+
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
 }
 
+int x = 0;
+bool reg = true;
+
 void loop() {
+    if (!digitalRead(12)) {
+      unsigned int v = 0;
+      for (int i = 2 ; i <= 9 ; ++i)
+        v = (v << 1) + digitalRead(i);
+      if (v == LOWREG) {
+        reg = false;
+      } else if (v == HIGHREG) {
+        reg = true;
+      } else {
+        if (reg) {
+          v = v ^ 3;
+        }
+        lcd.write(table[v]);
+        if (x == 16) {
+          lcd.scrollDisplayLeft();
+        } else {
+          ++x;
+        }
+      }
+      delay(50);
+    }
 }
